debfc8109525006bc1a223a8f4797db23c946ff3
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-04-30 11:45:09 -0400

    adding-models-sowri

--- src/models/Reservations.java
+++ src/models/Reservations.java
@@ -2,24 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Models;
+package models;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
 
 /**
  *
  * @author sampathkorturti
  */
 public class Reservations {
-    private static final String TAB = "      ";
-
-    private Date checkin;
+        private Date checkin;
     private Date checkout;
     private String status;
     private int cost;
-
 }
--- src/models/dep/DecorDep.java
+++ src/models/dep/DecorDep.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ModelsDep;
+package models.dep;
+
 
 /**
  *
  * @author sampathkorturti
  */
-public class HotelDep {
+public class DecorDep {
     
 }
