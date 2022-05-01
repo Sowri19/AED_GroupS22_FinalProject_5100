74804c61b91b28b744633009e8acb09c6c9976e9
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-04-30 23:40:10 -0400

    adding events arrays

--- src/Model/RS_BC_Events.java
+++ src/Model/RS_BC_Events.java
@@ -12,6 +12,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class RS_BC_Events {
-    
-}
+public class RS_BC_Events extends RS_BusinessCatalogue {
+
+    private Date date;
+    private List<RS_Supervisor> listOfSupervisor;
+    private List<RS_Event_BirthdayParty> listOfBirthdayParty;
+    private List<RS_Event_Wedding> listOfWeddingServices;
+    private List<RS_Event_Meetings> listOfMeetings;
+
+    public RS_BC_Events(String name, String contact) {
+        super(name, contact);
+        listOfSupervisor = new ArrayList<>();
+        listOfBirthdayParty = new ArrayList<>();
+        listOfWeddingServices = new ArrayList<>();
+        listOfMeetings = new ArrayList<>();
+
+    }
