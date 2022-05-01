0039d372fa05e4ae16957c24d2e85e03befb28fd
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-05-01 18:17:05 -0400

    adding_serviceagent

--- src/Model/RS_ServiceAgentOrganisation.java
+++ src/Model/RS_ServiceAgentOrganisation.java
@@ -2,12 +2,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author sampathkorturti
  */
-public class RS_ServiceAgentOrganisation {
-    
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RS_ServiceAgentOrganisation extends RS_Organization{
+
+    private List<RS_ServiceAgent> listOfServiceAgent;
+
+  public RS_ServiceAgentOrganisation(String name, String contact, String city) {
+        super(name, contact, city);
+        this .listOfServiceAgent = new ArrayList<>();
+    }
+  
+    public List<RS_ServiceAgent> getListOfServiceAgent() {
+        return listOfServiceAgent;
+    }
+
+    public void setListOfServiceAgent(List<RS_ServiceAgent> listOfServiceAgent) {
+        this.listOfServiceAgent = listOfServiceAgent;
+    }
+
+    public void addServiceAgent(String name, String city, String user, String password1) {
+        RS_ServiceAgent del = new RS_ServiceAgent(name, city, user, password1);
+        listOfServiceAgent.add(del);
+        System.out.println("Service Agent manager added is " + listOfServiceAgent.size());
+    }
+
+    public void deleteServiceAgent(RS_ServiceAgentOrganisation del) {
+     listOfServiceAgent.remove(del);
+    }
+
+    public void deleteSupervisor(RS_Supervisor supr) {
+      listOfSupervisor.remove(supr);
+    }
+
 }
+
