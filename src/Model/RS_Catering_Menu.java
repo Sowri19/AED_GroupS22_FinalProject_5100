79617258e3431e63afbb7cb8bbac295881b53c04
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-05-01 15:41:39 -0400

    adding cateringMenu class

---
+++ src/Model/RS_Catering_Menu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Pavithra Ramkumar
+ */
+
+public class RS_Catering_Menu {
+
+    private String details;
+    private int price;
+
+    public RS_Catering_Menu(String item, int price) {
+        this.details = item;
+        this.price = price;
+    }
