203323baa655fe1e59c477cd11df0190e80457f1
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-05-01 15:42:31 -0400

    menu getset

--- src/Model/RS_Catering_Menu.java
+++ src/Model/RS_Catering_Menu.java
@@ -18,3 +18,24 @@ public class RS_Catering_Menu {
         this.details = item;
         this.price = price;
     }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String toString() {
+        return details + " ($" + price + ")";
+    }
+}
