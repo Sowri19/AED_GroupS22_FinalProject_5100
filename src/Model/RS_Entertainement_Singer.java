d81c4b91dd589bfa6a59ecfb6a2cfb9e2e4f933c
Aishwarya <wagdarkar.a@northeaster.edu>
2022-05-01 16:49:34 -0400

    setlistofsingers-addanddeletesingers

--- src/Model/RS_Entertainement_Singer.java
+++ src/Model/RS_Entertainement_Singer.java
@@ -21,3 +21,38 @@ public class RS_Entertainment_Singer {
            this.username = username;
            this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
+}
--- src/Model/RS_Entertainement_SingerORG.java
+++ src/Model/RS_Entertainement_SingerORG.java
@@ -21,3 +21,21 @@ public class RS_Entertainment_SingerORG extends RS_Organization {
     }
 
 
+    public List<RS_Entertainment_Singer> getListOfSinger() {
+        return listOfSinger;
+    }
+
+    public void setListOfSinger(List<RS_Entertainment_Singer> listOfSinger) {
+        this.listOfSinger = listOfSinger;
+    }
+
+    public void addSinger(String name, String city, String user, String password1) {
+        RS_Entertainment_Singer singer = new RS_Entertainment_Singer(name, city, user, password1);
+        listOfSinger.add(singer);
+    }
+
+    public void deleteSupervisor(RS_Supervisor supr) {
+        listOfSupervisor.remove(supr);
+    }
+
+
