0a1409a446fd85513cd389b0b552c5f366e69f00
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-04-30 23:25:01 -0400

    adding-administrator-pavi

--- src/hello/Reservations.java
+++ src/hello/Reservations.java
@@ -2,12 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package models;
+package hello;
+
+import java.util.Date;
 
 /**
  *
  * @author sampathkorturti
  */
-public class Location {
-    
+public class Reservations {
+        private Date checkin;
+    private Date checkout;
+    private String status;
+    private int cost;
 }
--- src/hello/dep/resortDep.java
+++ src/hello/dep/resortDep.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package models;
-
-import java.util.Date;
+package hello.dep;
 
 /**
  *
  * @author sampathkorturti
  */
-public class Reservations {
-        private Date checkin;
-    private Date checkout;
-    private String status;
-    private int cost;
+public class resortDep {
+    
 }
