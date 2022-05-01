48e42023af776ead16ee49f07437e0b3a51c8c57
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-05-01 15:14:24 -0400

    adding getsettocarservices

--- src/Model/RS_CarService.java
+++ src/Model/RS_CarService.java
@@ -26,3 +26,36 @@ public class RS_CarService {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
