d64ed8c70d6bed80d2a2b52ee0aac6f78c2daac8
Aishwarya <wagdarkar.a@northeaster.edu>
2022-05-01 19:56:24 -0400

    implemented supervisor

--- src/Model/RS_Supervisor.java
+++ src/Model/RS_Supervisor.java
@@ -2,12 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author sampathkorturti
  */
-public class RS_Supervisor {
-    
+
+public class RS_Supervisor extends RS_Person {
+
+    private String contact;
+
+    public RS_Supervisor(String name,String username, String password) {
+        this.name = name;
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
 }
+
