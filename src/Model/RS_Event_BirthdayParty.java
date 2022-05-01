23624afcea4e4228799a61b56504d6f2908496bb
Aishwarya <wagdarkar.a@northeaster.edu>
2022-05-01 16:54:58 -0400

    intililisations and types

--- src/Model/RS_Event_BirthdayParty.java
+++ src/Model/RS_Event_BirthdayParty.java
@@ -2,12 +2,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author Pavithra Ramkumar
  */
-public class RS_Event_BirthdayParty {
-    
-}
+
+import java.util.List;
+
+public class RS_Event_BirthdayParty extends RS_Organization {
+
+    public RS_Event_BirthdayParty(String name, String contact, String city) {
+        super(name, contact, city);
+    }
+
+    public enum BirthdayPartyType {
+        DELUXE(1000, "DELUXE", "Includes basic Ballon Decor, Cake with Food for 100 guest"),
+        PREMIUM(2500, "PREMIUM", "Includes Return gift, Ballon Decor, Customised Cake and Food for 300 guest"),
+        ELITE(5000, "ELITE", "Includes PhotoBoth decor, baloon decor, customised Cake, Return Gift and food for 500 guest ");
+
+        private final int rate;
+        private final String name;
+        private final String description;
+
+        private BirthdayPartyType(int rate, String name, String description) {
+            this.rate = rate;
+            this.name = name;
+            this.description = description;
+        }
+
+        public int getRate() {
+            return rate;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String toString() {
+            return getName() + " (" + rate + "$ per night)";
+        }
+    }
+
--- src/Model/RS_Event_Meetings.java
+++ src/Model/RS_Event_Meetings.java
@@ -2,12 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author Pavithra Ramkumar
  */
-public class RS_Event_Meetings {
-    
-}
+
+public class RS_Event_Meetings extends RS_Organization {
+
+    public RS_Event_Meetings(String name, String contact, String city) {
+        super(name, contact, city);
+    }
+
+    public enum MeetingsType {
+        DELUXE(1200, "DELUXE", "Includes decor and standard food for 50 guest"),
+        PREMIUM(3000, "PREMIUM", "Includes flower decoration, two cuisines for Food and Projector for 150 guests"),
+        ELITE(5500, "ELITE", "Includes decoration,Multi cuisine Food , Projector and customised items for 300 guests");
+
+        private final int rate;
+        private final String name;
+        private final String description;
+
+        private MeetingsType(int rate, String name, String description) {
+            this.rate = rate;
+            this.name = name;
+            this.description = description;
+        }
+
