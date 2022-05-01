2bda22c1754dde5c49ceda56be9d0e7ede991afd
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-05-01 18:12:32 -0400

    getterandaddingthedelete

--- src/Model/RS_Organization.java
+++ src/Model/RS_Organization.java
@@ -26,3 +26,46 @@ public abstract class RS_Organization {
         this.listOfSupervisor = new ArrayList<>();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public List<RS_Supervisor> getListOfSupervisor() {
+        return listOfSupervisor;
+    }
+
+    public void setListOfSupervisor(List<RS_Supervisor> listOfSupervisor) {
+        this.listOfSupervisor = listOfSupervisor;
+    }
+    
+    public RS_Supervisor addSupervisor(String name, String city, String user, String password) {
+        RS_Supervisor supervisor = new RS_Supervisor(name, user, password);
+        listOfSupervisor.add(supervisor);
+        return supervisor;
+    }
+    
+    
+    public String toString() {
+        return name;
+    }
+}
--- src/Model/RS_Person.java
+++ src/Model/RS_Person.java
@@ -14,4 +14,27 @@ public abstract class RS_Person {
     protected String password;
     protected String username;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
