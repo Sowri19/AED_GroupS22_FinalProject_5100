724673daf239c623a9c54466cf7d2f3c0b662769
PavithraRamkumar <ramkumar.p@northeastern.edu>
2022-05-01 16:31:11 -0400

    adding-orgnisationsclassesusers

--- src/Model/RS_Entertainment_MagicianORG.java
+++ src/Model/RS_Entertainment_MagicianORG.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package hello;
+package Model;
 
 /**
  *
- * @author aishw
+ * @author Pavithra Ramkumar
  */
-public class LoungeServices {
+public class RS_Entertainment_MagicianORG {
     
 }
