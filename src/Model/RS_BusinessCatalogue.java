b901edf487d3dc08590e39019964154c314b95c3
Aishwarya <wagdarkar.a@northeaster.edu>
2022-05-01 14:39:59 -0400

    adding_businessClss

---
+++ src/Model/RS_BusinessCatalogue.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+/**
+ *
+ * @author aishw
+ */
+public class RS_BusinessCatalogue {
+
+    protected String name;
+    protected String contact;
+
+    public RS_BusinessCatalogue() {
+        
+    }
+    
+    public RS_BusinessCatalogue(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
