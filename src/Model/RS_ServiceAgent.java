d44dfe7198be1babc7e4ac8984618155ce217163
Aishwarya <wagdarkar.a@northeaster.edu>
2022-05-01 19:29:35 -0400

    service-agentand-orgobjintitial

--- src/Model/RS_ServiceAgent.java
+++ src/Model/RS_ServiceAgent.java
@@ -2,12 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author sampathkorturti
  */
-public class RS_ServiceAgent {
-    
-}
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RS_ServiceAgent extends RS_Person {
+
+    private String city;
+    List<RS_Catering_Order> list = new ArrayList<>();
+
+    public RS_ServiceAgent(String name, String city, String username, String password) {
+        this.name = name;
+        this.password = password;
+        this.username = username;
+        this.city = city;
+    }
+
--- src/Model/RS_ServiceAgentOrganisation.java
+++ src/Model/RS_ServiceAgentOrganisation.java
@@ -2,12 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package model;
 
 /**
  *
  * @author sampathkorturti
  */
-public class RS_ServiceAgentOrganisation {
-    
-}
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RS_ServiceAgentOrganisation extends RS_Organization{
+
+    private List<RS_ServiceAgent> listOfServiceAgent;
+
+  public RS_ServiceAgentOrganisation(String name, String contact, String city) {
+        super(name, contact, city);
+        this .listOfServiceAgent = new ArrayList<>();
+    }
+  
+    public List<RS_ServiceAgent> getListOfServiceAgent() {
+        return listOfServiceAgent;
+    }
+
+    public void setListOfServiceAgent(List<RS_ServiceAgent> listOfServiceAgent) {
+        this.listOfServiceAgent = listOfServiceAgent;
+    }
+
+
