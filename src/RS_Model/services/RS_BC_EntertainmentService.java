eeb474816ebcb76d55e24a0c47da324a41e092a3
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-05-01 21:47:25 -0400

    adding-Servicessettingparamaters

--- src/RS_Model/services/RS_BC_EntertainmentService.java
+++ src/RS_Model/services/RS_BC_EntertainmentService.java
@@ -8,6 +8,25 @@ package RS_Model.services;
  *
  * @author sampathkorturti
  */
-public class RS_BC_EntertainmentService {
-    
-}
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import model.RS_BC_Entertainment;
+
+public class RS_BC_EntertainmentService extends RS_Service {
+
+    public static enum EntertainmentServiceType {
+        MAGICIAN(60),
+        CHOREOGRAPHY(60),
+        SINGER(60);
+
+        private final int price;
+
+        private EntertainmentServiceType(int price) {
+            this.price = price;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
