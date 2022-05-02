7ded2fef7c084e7b428dd1c35dafae8a04ec4ba5
Sowri19 <98078922+Sowri19@users.noreply.github.com>
2022-05-01 21:53:54 -0400

    adding the parametersto the services

--- src/RS_Model/services/RS_BC_EventService.java
+++ src/RS_Model/services/RS_BC_EventService.java
@@ -27,3 +27,40 @@ public class RS_BC_EventService extends RS_Service {
 
     private Map<EventServiceType, Integer> EventServiceTypes;
 
+    public RS_BC_EventService(RS_BC_Events business, Date eventDate) {
+        super(business, ServiceType.EVENT, eventDate);
+        this.EventServiceTypes = new HashMap<>();
+    }
+
+    public Map<EventServiceType, Integer> getEventServiceTypes() {
+        return EventServiceTypes;
+    }
+
+    public void setEventServiceTypes(Map<EventServiceType, Integer> EventServiceTypes) {
+        this.EventServiceTypes = EventServiceTypes;
+    }
+
+    public void addService(EventServiceType type, int cost) {
+        EventServiceTypes.put(type, cost);
+        totalCost += cost;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("\nEvent service details:");
+        sb.append("\n").append(TAB).append("Organization conducting event: ").append(businessCatalogue);
+        sb.append("\n").append(TAB).append("Date of event: ").append(getDate());
+        sb.append("\n").append(TAB).append("Status: ").append(getStatus());
+
+        if (EventServiceTypes == null || EventServiceTypes.isEmpty()) {
+            sb.append("\n").append(TAB).append("No services selected for this booking.");
+        } else {
+            sb.append("\n").append(TAB).append("Below are the details of services included for the event:");
+            for (Map.Entry<EventServiceType, Integer> entry : EventServiceTypes.entrySet()) {
+                sb.append("\n").append(TAB).append(TAB)
+                        .append(String.format("Service type: %s, Cost: $%d", entry.getKey(), entry.getValue()));
+            }
+            sb.append("\n").append(TAB).append("Total cost for event: $").append(totalCost);
+        }
+        return sb.toString();
+    }
+}
--- src/RS_Model/services/RS_BC_ResortService.java
+++ src/RS_Model/services/RS_BC_ResortService.java
@@ -31,3 +31,54 @@ public class RS_BC_ResortService extends RS_Service {
             return price;
         }
     }
+
+    private RS_HallRoomList hallroomlist;
+    private List<ResortServiceType> ResortServices;
+
+    public RS_BC_ResortService(RS_BC_Resort resort) {
+        super(resort, RS_Service.ServiceType.RESORT, DateUtils.now());
+        this.ResortServices = new ArrayList<>();
+        this.hallroomlist = new RS_HallRoomList();
+    }
+
+    public List<ResortServiceType> getResortServices() {
+        return ResortServices;
+    }
+
+    public void setResortServices(List<ResortServiceType> ResortServices) {
+        this.ResortServices = ResortServices;
+    }
+
+    public RS_HallRoomList getHallroomlist() {
+        return hallroomlist;
+    }
+
+    public void setHallroomlist(RS_HallRoomList hallroomlist) {
+        this.hallroomlist = hallroomlist;
+    }
+
+    public void addService(RS_BC_ResortService.ResortServiceType type) {
+        ResortServices.add(type);
+        totalCost += type.price;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("\nResort service details:");
+        sb.append("\n").append(TAB).append("Resort: ").append(businessCatalogue);
+        sb.append("\n").append(TAB).append("Date of appointment: ").append(getDate());
+        sb.append("\n").append(TAB).append("Status: ").append(getStatus());
+
+        if (ResortServices == null || ResortServices.isEmpty()) {
+            sb.append("\n").append(TAB).append("No services selected for this booking.");
+        } else {
+            sb.append("\n").append(TAB).append("Below are the details of services included for your booking:");
+            for (RS_BC_ResortService.ResortServiceType service : ResortServices) {
+                sb.append("\n").append(TAB).append(TAB)
+                        .append(String.format("Service type: %s, Cost: $%d", service.toString(), service.getPrice()));
+            }
+            sb.append("\n").append(TAB).append("Total cost for hotel: $").append(totalCost);
+        }
+        return sb.toString();
+    }
+
+}
